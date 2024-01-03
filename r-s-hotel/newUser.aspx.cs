b33ad4b0e61618e1978ea93cf25b6ac9f69f7538
Ronak Parmar <114209128+RonakParmar21@users.noreply.github.com>
2024-01-03 05:21:33 +0000

    add email verification code

--- r-s-hotel/newUser.aspx.cs
+++ r-s-hotel/newUser.aspx.cs
@@ -4,23 +4,68 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.Net;
 
 namespace r_s_hotel
 {
     public partial class newUser : System.Web.UI.Page
     {
+        static string activationgcode;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+     
         }
-
-       
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            string nm = name.Text;
+            //       smtpClient.Credentials = new NetworkCredential("parmarronak21112003", "edgk duom pynn eyzj");
+            //Random random = new Random();
+            //activationgcode = random.Next(1001, 9999).ToString();
 
-            //Response.Write("<script>alert('"+nm+"')</script>");
+            Random random = new Random();
+            // Generate a random six-digit code
+            activationgcode = random.Next(100000, 1000000).ToString();
+            // Print or use the generated code
+            //Console.WriteLine("Generated Six-Digit Code: " + sixDigitCode);
+
+
+
+
+
+
+            sendcode();
+
+
+
+            Response.Redirect("verifyEmail.aspx");
+        }
+
+        private void sendcode()
+        {
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential("parmarronak21112003@gmail.com", "edgk duom pynn eyzj");
+            smtp.EnableSsl = true;
+            MailMessage msg = new MailMessage();
+            msg.Subject = "activation code to verify email address";
+            msg.Body = "Your code is : " + activationgcode;
+            string toaddress = email.Text;
+            msg.To.Add(toaddress);
+            string fromaddress = "rshotel : <parmarronak21112003@gmail.com>";
+            msg.From = new MailAddress(fromaddress);
+            try
+            {
+                smtp.Send(msg);
+            } 
+            catch(Exception e)
+            {
+                //throw;
+                Console.Write(e);
+            }
         }
     }
 
---
+++ r-s-hotel/verifyEmail.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="verifyEmail.aspx.cs" Inherits="r_s_hotel.verifyEmail" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="email verify page"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
