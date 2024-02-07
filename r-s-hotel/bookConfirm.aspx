7e4e98da626e2c25cbab180aedd0775b3e90ba95
Ronak Parmar <114209128+RonakParmar21@users.noreply.github.com>
2024-02-07 19:23:08 +0530

    book confirm page design done

--- r-s-hotel/bookConfirm.aspx
+++ r-s-hotel/bookConfirm.aspx
@@ -2,4 +2,26 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div style="display: flex; justify-content: center; align-items: center; margin-top: 20px; margin-bottom: 20px;">
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="4" ForeColor="Black" GridLines="Horizontal" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            <FooterStyle BackColor="#CCCC99" ForeColor="Black" />
+            <HeaderStyle BackColor="#333333" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="Black" HorizontalAlign="Right" />
+            <SelectedRowStyle BackColor="#CC3333" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F7F7F7" />
+            <SortedAscendingHeaderStyle BackColor="#4B4B4B" />
+            <SortedDescendingCellStyle BackColor="#E5E5E5" />
+            <SortedDescendingHeaderStyle BackColor="#242121" />
+
+            <Columns>
+                <asp:BoundField DataField="Parameter" HeaderText="Parameter" ControlStyle-Font-Bold="true" />
+                <asp:BoundField DataField="Value" HeaderText="Value" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    <center>
+        <div style="margin-bottom: 130px;">
+            <asp:Button ID="Button1" runat="server" Text="Confirm Order" OnClick="Button1_Click" CssClass="btn btn-sm btn-dark rounded py-2 px-4" />
+        </div>
+    </center>
 </asp:Content>
--- r-s-hotel/bookConfirm.aspx.cs
+++ r-s-hotel/bookConfirm.aspx.cs
@@ -4,50 +4,118 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace r_s_hotel
 {
     public partial class bookConfirm : System.Web.UI.Page
     {
+        //    int tday, roomqty, uid, rid, roomPrice, totalPrice;
+        //  string request, payment, unm, uem, umo, cin, cout, rtype, tperson;
+
+        int tday, roomqty, uid, rid, roomPrice, totalPrice;
+        string request, payment, unm, uem, umo, cin, cout, rtype, tperson;
         protected void Page_Load(object sender, EventArgs e)
         {
             //int tday = Convert.ToInt32(Request.QueryString["totalDays"]);
             //string tday = Request.QueryString["totalDays"];
-            int tday = Convert.ToInt32(Request.QueryString["totalDays"]);
-            int roomqty = Convert.ToInt32(Request.QueryString["roomqt"]);
-            string request = Request.QueryString["re"];
-            string payment = Request.QueryString["pm"];
-            int uid = Convert.ToInt32(Request.QueryString["userId"]);
-            string unm = Request.QueryString["userName"];
-            string uem = Request.QueryString["userEmail"];
-            string umo = Request.QueryString["userMobile"];
-            string cin = Request.QueryString["checkIn"];
-            string cout = Request.QueryString["checkOut"];
+            tday = Convert.ToInt32(Request.QueryString["totalDays"]);
+            roomqty = Convert.ToInt32(Request.QueryString["roomqt"]);
+            request = Request.QueryString["re"];
+            payment = Request.QueryString["pm"];
+            uid = Convert.ToInt32(Request.QueryString["userId"]);
+            unm = Request.QueryString["userName"];
+            uem = Request.QueryString["userEmail"];
+            umo = Request.QueryString["userMobile"];
+            cin = Request.QueryString["checkIn"];
+            cout = Request.QueryString["checkOut"];
 
-            int rid = Convert.ToInt32(Request.QueryString["roomId"]);
-            int roomPrice = Convert.ToInt32(Request.QueryString["rp"]);
-            string rtype = Request.QueryString["roomType"];
-            string tperson = Request.QueryString["tp"];
-            int totalPrice = roomPrice * tday * roomqty;
+            rid = Convert.ToInt32(Request.QueryString["roomId"]);
+            roomPrice = Convert.ToInt32(Request.QueryString["rp"]);
+            rtype = Request.QueryString["roomType"];
+            tperson = Request.QueryString["tp"];
+            totalPrice = roomPrice * tday * roomqty;
 
-            Response.Write("<h1>User id '"+uid+"'</h1>");
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Parameter", typeof(string));
+            dt.Columns.Add("Value", typeof(string));
+
+            // Continue adding rows for each parameter
+            dt.Rows.Add("User Id", uid);
+            dt.Rows.Add("User Name", unm);
+            dt.Rows.Add("User Email", uem);
+            dt.Rows.Add("User Mobile", umo);
+            //dt.Rows.Add("Room Id", rid);
+            dt.Rows.Add("Room Type", rtype);
+            dt.Rows.Add("Room Quantity", roomqty);
+            dt.Rows.Add("Total Person", tperson);
+            dt.Rows.Add("CheckIN Date", cin);
+            dt.Rows.Add("Checkout Date", cout);
+            dt.Rows.Add("Total Days", tday);
+            dt.Rows.Add("Room Price/Night", roomPrice);
+            dt.Rows.Add("Total Price", totalPrice);
+            dt.Rows.Add("Payment Status", payment);
+            dt.Rows.Add("Special Request", request);
+
+
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+
+
+
+
+            /*Response.Write("<h1>User id '"+uid+"'</h1>");
             Response.Write("<h1>User name '" + unm + "'</h1>");
             Response.Write("<h1>User email '" + uem + "'</h1>");
             Response.Write("<h1>User mobile '" + umo + "'</h1>");
-
             Response.Write("<h1>Room id '" + rid + "'</h1>");
             Response.Write("<h1>Room Type '" + rtype + "'</h1>");
             Response.Write("<h1>Total Day '" + tday + "'</h1>");
-
             Response.Write("<h1>Checkin date '" + cin + "'</h1>");
             Response.Write("<h1>Checkout date '" + cout + "'</h1>");
             Response.Write("<h1>Room Price '" + roomPrice + "'</h1>");
             Response.Write("<h1>Total Room Price '" + totalPrice + "'</h1>");
-
             Response.Write("<h1>Total Person = '"+tperson+"'</h1>");
             Response.Write("<h1>Room Qty = '" + roomqty + "'</h1>");
             Response.Write("<h1>Payment Method = '" + payment + "'</h1>");
             Response.Write("<h1>Request = '" + request + "'</h1>");
+            */
+        }
+
+
+        /*private DataTable CreateDataTable()
+        {
+            // Create a new DataTable.
+            DataTable dt = new DataTable();
+
+            // Define columns in the DataTable.
+            dt.Columns.Add("Id", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Email", typeof(string));
+
+            // Assuming you have some variables with data.
+            int id = 1;
+            string name = "John Doe";
+            string email = "john.doe@example.com";
+
+            // Add a row to the DataTable with these variables.
+            dt.Rows.Add(id, name, email);
+
+            // If you have more data, you can add more rows here.
+            // For example, another set of variables or data from a loop.
+
+            return dt;
+        }*/
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
 
         }
     }
